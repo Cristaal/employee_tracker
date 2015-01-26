d9b03ba29f111ea97570af867e67cac74d4705d2
Crystal Contreras-Grossman & Arsi <student@epicodus.com>
2015-01-26 14:35:33 -0800

    add methods for displaying divisions and employees

--- app.rb
+++ app.rb
@@ -14,23 +14,27 @@ get('/') do
 end
 
 post("/employee") do
+  @employees = Employee.all()
+  @divisions = Division.all()
   name = params.fetch("name")
   division_id = params.fetch("division_id").to_i()
   @employee = Employee.new({:name => name, :division_id => division_id})
   @employee.save()
-  redirect back
+  erb(:index)
 end
 
 
 post("/division") do
+  @employees = Employee.all()
+  @divisions = Division.all()
   name = params.fetch("name")
   @division = Division.new({:name => name})
   @division.save()
-  redirect back
+  erb(:index)
 end
 
 post("/division_list") do
-  @employees = Employee.all() 
+  @employees = Employee.all()
   division_id = params.fetch("division_id").to_i()
   @divisions = Division.all()
   @division_employees = Employee.where(division_id: division_id)
